22ef55b5a0097ebfee67eba701e67b1019b444bf
Blake Hachen <blakehachen@ksu.edu>
2022-01-24 20:34:31 -0600

    initial commit

---
+++ game-project-0/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace game_project_0
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new GameProject0())
+                game.Run();
+        }
+    }
+}
